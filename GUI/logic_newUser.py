9adcb730484b10399c7d3f255da2cfec5dab3343
HolyShine <xuebiha@gmail.com>
2019-11-11 16:57:05 +0800

    数据库迁移到了mysql

--- GUI/logic_newUser.py
+++ GUI/logic_newUser.py
@@ -33,46 +33,56 @@ class logicNewUser(Ui_newUser, QDialog):
         elif self.edit_phone.text() == '':
             QMessageBox.warning(self, '提示','新员工联系方式不能为空！', QMessageBox.Yes,QMessageBox.Yes)
         else:
-            sql = 'INSERT INTO mem_info (mem_name, mem_gender, mem_parent, mem_phone, mem_type) VALUES(\'{}\',\'{}\',\'{}\',{},{})'.format(
-                                    self.edit_name.text(), 
-                                    self.cb_gender.currentText(), 
-                                    self.edit_parent.text(), 
-                                    self.edit_phone.text(),
-                                    self.cb_type.currentText(),
-                                )
+            if self.cb_mem_type.currentText() == '学生':
+                sql = 'INSERT INTO mem_info (mem_name, mem_gender, mem_parent, mem_phone, mem_type) VALUES(\'{}\',\'{}\',\'{}\',{},{})'.format(
+                                        self.edit_name.text(), 
+                                        self.cb_gender.currentText(), 
+                                        self.edit_parent.text(), 
+                                        self.edit_phone.text(),
+                                        self.cb_type.currentText(),
+                                    )
 
 
-            
-            hint = '姓名：{}\n性别：{}\n家长：{}\n联系方式：{}\n办卡类型：{}'.format(
-                                    self.edit_name.text(), 
-                                    self.cb_gender.currentText(), 
-                                    self.edit_parent.text(), 
-                                    self.edit_phone.text(),
-                                    self.cb_type.currentText()
-                                )
+                
+                hint = '学员姓名：{}\n性别：{}\n家长：{}\n联系方式：{}\n办卡类型：{}'.format(
+                                        self.edit_name.text(), 
+                                        self.cb_gender.currentText(), 
+                                        self.edit_parent.text(), 
+                                        self.edit_phone.text(),
+                                        self.cb_type.currentText()
+                                    )
+            else:
+                sql = 'INSERT INTO coach(coa_name, coa_phone) VALUES(\'{}\',{})'.format(
+                                        self.edit_name.text(), 
+                                        self.edit_phone.text(),
+                                    )   
+                hint = '教练姓名：{}\n联系方式：{}'.format(
+                                        self.edit_name.text(), 
+                                        self.edit_phone.text()
+                                    )             
             reply = QMessageBox.warning(self, '确认信息',hint, QMessageBox.Yes|QMessageBox.No, QMessageBox.Yes)
             print(sql)
             if reply == QMessageBox.Yes:
                 try:
-                    # conn = pymysql.connect(
-                    #     host='localhost',
-                    #     user='root',
-                    #     password='1230456',
-                    #     database = 'user_info_db',
-                    #     charset='utf8'
-                    # )
-                    # cursor = conn.cursor()
-                    # cursor.execute(sql)
-                    # conn.commit()
-                    # QMessageBox.information(self, '提示', '录入成功！',QMessageBox.Ok,QMessageBox.Ok)
-
-                    # conn.close()
-                    conn = sqlite3.connect('meminfo.db')
-                    c = conn.cursor()   
-                    c.execute(sql)
+                    conn = pymysql.connect(
+                        host='localhost',
+                        user='root',
+                        password='1230456',
+                        database = 'meminfo',
+                        charset='utf8'
+                    )
+                    cursor = conn.cursor()
+                    cursor.execute(sql)
                     conn.commit()
-                    conn.close()
                     QMessageBox.information(self, '提示', '录入成功！',QMessageBox.Ok,QMessageBox.Ok)
+
+                    conn.close()
+                    # conn = sqlite3.connect('meminfo.db')
+                    # c = conn.cursor()   
+                    # c.execute(sql)
+                    # conn.commit()
+                    # conn.close()
+                    # QMessageBox.information(self, '提示', '录入成功！',QMessageBox.Ok,QMessageBox.Ok)
                 except:
                     QMessageBox.critical(self,'错误','数据库异常！',QMessageBox.Ok,QMessageBox.Ok)
             else:
