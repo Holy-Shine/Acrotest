fbad2892dada3b236df70b2ce635dfdba3508754
Emmons <243633699@qq.com>
2019-11-12 17:08:00 +0800

    增加录入新学员系统

--- newMember/UI_NewMember.py
+++ newMember/UI_NewMember.py
@@ -8,7 +8,7 @@
 
 from PyQt5 import QtCore, QtGui, QtWidgets
 
-class Ui_Form(object):
+class Ui_NewMember(object):
     def setupUi(self, Form):
         Form.setObjectName("Form")
         Form.resize(581, 566)
---
+++ newMember/logic_MemberCAMChose.py
@@ -0,0 +1,27 @@
+from PyQt5.QtWidgets import QDialog,QMessageBox,QLineEdit,QApplication
+
+from UI_Qiandao_camera import UI_Qiandao_camera
+from PyQt5.QtCore import pyqtSignal
+import os,sys
+
+class LogicMemberCAMChose(UI_Qiandao_camera,QDialog):
+    mySignal = pyqtSignal(str)
+    def __init__(self,cameranum):
+        super().__init__()
+        self.setupUi(self)
+        self.cameranum = cameranum
+
+        self.init()
+        self.slot_init()
+
+    def init(self):
+        for i in range(self.cameranum):
+            self.comboBox.addItem(str(i))
+
+    def slot_init(self):
+        self.bt_confrim.clicked.connect(self.chosecam)
+
+    def chosecam(self):
+        content = self.comboBox.currentText()
+        self.mySignal.emit(content)  # 发射信
+        self.close()
