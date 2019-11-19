f231e5ce066a39ee71bf8936c1d31b291095de16
HolyShine <xuebiha@gmail.com>
2019-11-19 18:13:34 +0800

    更新了排课系统界面逻辑

--- GUI/logic_updateClass.py
+++ GUI/logic_updateClass.py
@@ -1,8 +1,10 @@
 
 
 from Ui_updateClass import Ui_updateClass
-from PyQt5.QtWidgets import QDialog,QMessageBox,QTableView,QHeaderView
+from PyQt5.QtWidgets import QDialog,QMessageBox,QTableView,QHeaderView, QListWidget, QStackedWidget
 from PyQt5.QtGui import QStandardItemModel,QStandardItem
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
 
 import sqlite3, pymysql
 
@@ -11,6 +13,7 @@ class logicUpdateClass(Ui_updateClass, QDialog):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
+        self.beatify()
 
         self.current_row = -1
         self.cb_coach.setCurrentIndex(0)
@@ -18,6 +21,12 @@ class logicUpdateClass(Ui_updateClass, QDialog):
         self.tv_show_mem.setSelectionBehavior(QTableView.SelectRows) # 选中行
         
 
+        # 设置背景
+        # paletter = QPalette()
+        # paletter.setBrush(QPalette.Background, QBrush(QPixmap('resource/paike_bg.png')))
+        # self.setPalette(paletter)
+
+        self.weekdays = ['周一','周二','周三','周四','周五','周六','周日']
         self.headers = ['联系方式','姓名']
         self.data_model = QStandardItemModel()
         self.data_model.setHorizontalHeaderLabels(self.headers)
@@ -25,13 +34,26 @@ class logicUpdateClass(Ui_updateClass, QDialog):
         self.tv_show_mem.setModel(self.data_model)
         self.tv_show_mem.selectionModel().selectionChanged.connect(self.row_sel_change) # 选中事件
 
-        self.tv_show_mem.horizontalHeader().setSectionResizeMode(QHeaderView.Interactive)
+        self.tv_show_mem.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
         
         self.btn_flush.clicked.connect(self.on_button_flush)
         self.btn_confirm.clicked.connect(self.on_button_confirm)
 
+        self.cb_day_times = []
+
+    def beatify(self):
+        with open('./left_list.qss','r') as f:
+            self.list_style = f.read()
+        
+        self.listWidget.setSpacing(10)
+        self.listWidget.setStyleSheet(self.list_style)
+        self.listWidget.currentRowChanged.connect(self.stackedWidget.setCurrentIndex)   # list和右边窗口index绑定
+        
+        self.listWidget.setFrameShape(QListWidget.NoFrame)
+
     def myshow(self):
         self.setupItem()
+
         self.show()
 
     def on_button_confirm(self):
@@ -40,10 +62,13 @@ class logicUpdateClass(Ui_updateClass, QDialog):
         else:
             # 检查是否未选择排课日期
             flag = True
-            for cb in self.cb_day.qCheckBox:
+            for cb in self.cb_day_times:
                 flag = flag and (not cb.isChecked())
             if flag:
                 QMessageBox.warning(self, '提示','未选择排课日期!', QMessageBox.Yes, QMessageBox.Yes)
+            
+            elif self.cb_coach.currentText=='未选择':
+                QMessageBox.warning(self, '提示','未选择教练!', QMessageBox.Yes, QMessageBox.Yes)
             
             else:
                 sql_T = []
@@ -52,11 +77,13 @@ class logicUpdateClass(Ui_updateClass, QDialog):
                     self.le_choose_phone.text(),
                     self.cb_coach.currentText()
                     )
-
-                for i, cb in enumerate(self.cb_day.qCheckBox):
+                base = 10
+                for i, cb in enumerate(self.cb_day_times):
                     if cb.isChecked():
-                        str_confirm+=(cb.text()+': '+self.cb_day.qComboBox[i].currentText()+'\n   ')
-                        sql_T.append('T'+str(i+1)+self.cb_day.qComboBox[i].currentText()[:2])
+                        day = i//12
+                        time = str(base+i%12)
+                        str_confirm+=(self.weekdays[day]+': '+time+':00'+'\n   ')
+                        sql_T.append('T'+str(day)+time)
 
                 reply = QMessageBox.warning(self, '确认信息',str_confirm, QMessageBox.Yes|QMessageBox.No, QMessageBox.Yes)
 
@@ -86,15 +113,17 @@ class logicUpdateClass(Ui_updateClass, QDialog):
                         cursor = conn.cursor()
                         cursor.execute(sql)
                         conn.commit()
-                        QMessageBox.information(self, '提示', '录入成功！',QMessageBox.Ok,QMessageBox.Ok)
-
                         conn.close()
                         # conn = sqlite3.connect('meminfo.db')
                         # c = conn.cursor()   
                         # c.execute(sql)
                         # conn.commit()
                         # conn.close()
+                        for i in range(2):
+                            self.data_model.setData(self.data_model.index(self.current_row, i), QBrush(Qt.green), Qt.BackgroundColorRole)
                         QMessageBox.information(self, '提示', '排课成功！',QMessageBox.Ok,QMessageBox.Ok)
+                        
+                        
                         
                         #删除选中行
                         # self.tv_show_mem.clearSelection()
@@ -118,9 +147,15 @@ class logicUpdateClass(Ui_updateClass, QDialog):
         else:
             self.le_choose_name.setText('')
             self.le_choose_phone.setText('')
+
+        self.clean_up()
         
         
         
+    def clean_up(self):
+        # 清除工作
+        for i in range(84):
+            self.cb_day_times[i].setChecked(False)
 
     def setupItem(self):
         sql = 'SELECT coa_name FROM coach'
@@ -146,6 +181,13 @@ class logicUpdateClass(Ui_updateClass, QDialog):
             print(e)
             QMessageBox.critical(self,'错误','数据库异常！无法连接到教练数据库',QMessageBox.Ok,QMessageBox.Ok)       
 
+
+
+        # 将checkbox全部放入到cbs中
+        for i in range(84):
+            self.cb_day_times.append(eval('self.'+'cb_{}'.format(i)))
+
+        self.clean_up()
 
     def on_button_flush(self):
         # 清空表
@@ -180,6 +222,12 @@ class logicUpdateClass(Ui_updateClass, QDialog):
                 conn.close()
             except:
                 QMessageBox.critical(self,'错误','数据库异常！',QMessageBox.Ok,QMessageBox.Ok)
+
+            # 若未更新不为空，则确认按钮可用
+            if len(self.data)>0:
+                self.btn_confirm.setEnabled(True)
+            else:
+                self.btn_confirm.setEnabled(False)
                 
 
         
