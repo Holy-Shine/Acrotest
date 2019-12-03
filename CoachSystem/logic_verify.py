2dbe4655c77418f3593b624f820d3c20c2a6460d
HolyShine <xuebiha@gmail.com>
2019-12-03 14:26:56 +0800

    教练系统二级密码

---
+++ CoachSystem/logic_verify.py
@@ -0,0 +1,22 @@
+
+from PyQt5.QtWidgets import QDialog,QMessageBox
+from PyQt5.QtCore import Qt
+from CoachSystem.Ui_verify import Ui_verify
+class logicVerify(Ui_verify, QDialog):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)  
+
+        self.btn_confirm.clicked.connect(self.on_button_verify)
+        self.btn_confirm.setShortcut(Qt.Key_Return)  
+
+
+    def myshow(self):
+        self.le_pwd.setText('')
+
+    def on_button_verify(self):
+        pwd = self.le_pwd.text()
+        if pwd == 'Jessie123':
+            self.accept()
+        else:
+            QMessageBox.warning(self, '提示','二级密码错误！', QMessageBox.Yes, QMessageBox.Yes)
