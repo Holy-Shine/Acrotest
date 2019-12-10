46895a439cd12a194050a33281f802e3882528bb
Holy-Shine <xuebiha@gmail.com>
2019-12-10 21:17:01 +0800

    修复bug，增加判断课时次数逻辑

--- Date2Week/DateAndWeek.py
+++ Date2Week/DateAndWeek.py
@@ -3,15 +3,12 @@ from datetime import datetime
 
 
 def FromDatetoWeek(year,month,day):
-    try:
-        date_day = '{}{}{}'.format(year,month,day)
-        week = int(datetime.strptime(date_day, "%Y%m%d").strftime("%W")) + 1
-        weekday = '7' \
-            if datetime.strptime(date_day, "%Y%m%d").strftime("%w") == '0' \
-            else datetime.strptime(date_day,"%Y%m%d").strftime("%w")
-        return week,weekday
-    except Exception as e:
-        print(e)
+    date_day = '{}{}{}'.format(year,month,day)
+    week = int(datetime.strptime(date_day, "%Y%m%d").strftime("%W")) + 1
+    weekday = '7' \
+        if datetime.strptime(date_day, "%Y%m%d").strftime("%w") == '0' \
+        else datetime.strptime(date_day,"%Y%m%d").strftime("%w")
+    return week,weekday
 
 
 def FromWeektoDate(year,week,day):
@@ -34,29 +31,26 @@ def getWeekNum(year):
 
 #返回某年第一周的第一天是周几
 def getFirstWeekDate(year):
-    try:
-        _, weekday = FromDatetoWeek(year, '01', '01')
-        return int(weekday)
-    except Exception as e:
-        print(e)
+    _, weekday = FromDatetoWeek(year, '01', '01')
+    return weekday
 
 #返回某年最后一周是周几
 def getLastWeekDate(year):
     _, weekday = FromDatetoWeek(year, '12', '31')
-    return  int(weekday)
+    return  weekday
 
 if __name__ == '__main__':
-    y = '2019'
-    m = '1'
-    d = '1'
+    # y = '2019'
+    # m = '12'
+    # d = '03'
     #
     #
-    week,weekday = FromDatetoWeek(y,m,d)
-    print('{}年第{}周的周{}'.format(2019,week,weekday))
-    year,mon,day = FromWeektoDate(2019,int(week)-1,int(weekday))
-    print('{}年第{}月{}日'.format(year, mon, day))
+    # week,weekday = FromDatetoWeek(y,m,d)
+    # print('{}年第{}周的周{}'.format(2019,week,weekday))
+    # year,mon,day = FromWeektoDate(2019,int(week)-1,int(weekday))
+    # print('{}年第{}月{}日'.format(year, mon, day))
     # print(getWeekNum(2022))
 
-    # year = 2020
-    # print("{}年的第一天是周{}".format(year,getFirstWeekDate(year)))
-    # print("{}年的最后一天是周{}".format(year,getLastWeekDate(year)))
+    year = 2020
+    print("{}年的第一天是周{}".format(year,getFirstWeekDate(year)))
+    print("{}年的最后一天是周{}".format(year,getLastWeekDate(year)))
